8645fa763464d1d19500fbc4e1f227328bf3381c
tanhoang <tan.hoang@anfin.vn>
2022-07-05 16:17:29 +0700

    first commit

---
+++ lib/method_channel_in_app_review.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'in_app_review_platform_interface.dart';
+
+/// An implementation of [InAppReviewPlatform] that uses method channels.
+class MethodChannelInAppReview extends InAppReviewPlatform {
+  MethodChannel _channel = MethodChannel('dev.britannio.flutter_rating_app');
+  Platform _platform = Platform();
+
+  @visibleForTesting
+  set channel(MethodChannel channel) => _channel = channel;
+
+  @visibleForTesting
+  set platform(Platform platform) => _platform = platform;
+
+  @override
+  Future<bool> isAvailable() async {
+    if (kIsWeb) return false;
+    return _channel
+        .invokeMethod<bool>('isAvailable')
+        .then((available) => available ?? false, onError: (_) => false);
+  }
+
+  @override
+  Future<bool> requestReview() async {
+    return await _channel.invokeMethod('requestReview');
+  }
+
+  @override
+  Future<void> openStoreListing({
+    String? appStoreId,
+    String? microsoftStoreId,
+  }) async {
+    final bool isIOS = Platform.isIOS;
+    final bool isMacOS = Platform.isMacOS;
+    final bool isAndroid = Platform.isAndroid;
+    final bool isWindows = Platform.isWindows;
+
+    if (isIOS || isMacOS) {
+      await _channel.invokeMethod(
+        'openStoreListing',
+        ArgumentError.checkNotNull(appStoreId, 'appStoreId'),
+      );
+    } else if (isAndroid) {
+      await _channel.invokeMethod('openStoreListing');
+    } else if (isWindows) {
+      ArgumentError.checkNotNull(microsoftStoreId, 'microsoftStoreId');
+      await _launchUrl(
+        'ms-windows-store://review/?ProductId=$microsoftStoreId',
+      );
+    } else {
+      throw UnsupportedError(
+        'Platform(${Platform.operatingSystem}) not supported',
+      );
+    }
+  }
+
+  Future<void> _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceSafariVC: false, forceWebView: false);
+    }
+  }
+}
